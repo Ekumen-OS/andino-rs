0f86812f7541461543ee16881d91100b3e413b87
Franco Cipollone <franco.c@ekumenlabs.com>
2025-04-29 03:29:36 +0000

    Add basic layout and CI.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

---
+++ andino-core/src/lib.rs
@@ -0,0 +1,16 @@
+// TODO(francocipollone): Remove this when the library is ready
+// Dummy library to test the workspace
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
