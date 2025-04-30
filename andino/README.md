d4b8acfe855381ae8a7cd3846a8fa0ebb36ab263
Franco Cipollone <franco.c@ekumenlabs.com>
2025-04-30 20:28:02 +0000

    Creates HwSerialConnection abstraction.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

---
+++ andino/README.md
@@ -0,0 +1,3 @@
+# andino
+
+TODO
---
+++ andino/examples/02_hardware_serial_connection.rs
@@ -0,0 +1,110 @@
+// ***************************************************************************
+// About
+// ***************************************************************************
+//
+//! Example of how to use the `HwSerialConnection` struct to communicate with
+//! the underlying hardware via serial port.
+//! Available commands to send to the serial connection:
+//! - `ReadEncoderValues`
+//! - `SetMotorValues <left> <right>`
+//! - `SetPIDValues <kp> <ki> <kd> <ko>`
+//!
+//! cargo run --example 02_hardware_serial_connection
+//!
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Args {
+    /// Serial device name.
+    #[arg(short, long, default_value_t = String::from("/dev/ttyUSB0"))]
+    serial_device: String,
+
+    /// Baud rate for the serial connection.
+    #[arg(short, long, default_value_t = 57600)]
+    baud_rate: u32,
+
+    /// Timeout for the serial connection in milliseconds.
+    #[arg(short, long, default_value_t = 3000)]
+    timeout: u64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let args = Args::parse();
+
+    log::info!("Creates an instance of HwSerialConnection");
+    // Create a new serial connection
+    let mut serial_connection =
+        andino::core::comm::HwSerialConnection::new(args.serial_device, args.baud_rate, args.timeout)?;
+
+    log::info!("Waits 3 seconds for the serial connection to be established");
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
+    loop {
+        // Ask the user for a command, the input can have several arguments
+        // For example: "SetMotorValues 100 200"
+        println!(
+            "Available commands:
+            \t - ReadEncoderValues
+            \t - SetMotorValues <tps_left> <tps_right>
+            \t - SetPIDValues <kp> <ki> <kd> <ko>
+            Note: <tps_left> and <tps_right> are ticks(encoder) per second."
+        );
+        println!("* Enter a command (or 'exit' to quit):");
+        let mut input = String::new();
+
+        if std::io::stdin().read_line(&mut input).is_err() {
+            println!("Error reading input, try again.");
+            continue;
+        }
+        let input = input.trim();
+        if input == "exit" {
+            break;
+        }
+        // Split the input into arguments
+        let input_args = input.split_whitespace().collect::<Vec<&str>>();
+
+        // Check if the user wants to exit
+        // Parse the command
+        let command = match input_args[0] {
+            "ReadEncoderValues" => andino::core::comm::SerialCommands::ReadEncoderValues,
+            "SetMotorValues" => {
+                if input_args.len() < 3 {
+                    println!("SetMotorValues command requires two arguments.");
+                    continue;
+                }
+                andino::core::comm::SerialCommands::SetMotorValues {
+                    left: input_args[1].parse::<i32>().unwrap_or(0),
+                    right: input_args[2].parse::<i32>().unwrap_or(0),
+                }
+            }
+            "SetPIDValues" => {
+                if input_args.len() < 5 {
+                    println!("SetPIDValues command requires four arguments.");
+                    continue;
+                }
+                andino::core::comm::SerialCommands::SetPIDValues {
+                    kp: input_args[1].parse::<f32>().unwrap_or(0.0),
+                    ki: input_args[2].parse::<f32>().unwrap_or(0.0),
+                    kd: input_args[3].parse::<f32>().unwrap_or(0.0),
+                    ko: input_args[4].parse::<f32>().unwrap_or(0.0),
+                }
+            }
+            _ => {
+                println!("Unknown command: {}\nTry again", input_args[0]);
+                continue;
+            }
+        };
+
+        println!("* Sending command: {:?}", command);
+        // Example of sending a command to the serial connection
+        let response = serial_connection.send_command(command)?;
+        println!("* RESPONSE: {:?}", response);
+        println!();
+        println!();
+    }
+    println!("* Exiting the program");
+    Ok(())
+}
