e69e0109e521aa8b66f5d37ce2d17c771eeaed7b
Franco Cipollone <franco.c@ekumenlabs.com>
2025-07-14 17:46:22 +0000

    Minor fix to __main__ file.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- dora_node_hub/dora_gemini_diff_drive_navigation/dora_gemini_diff_drive_navigation/__main__.py
+++ dora_node_hub/dora_gemini_diff_drive_navigation/dora_gemini_diff_drive_navigation/__main__.py
@@ -1,6 +1,9 @@
 """Main entry point for the dora_gemini_diff_drive_navigation package."""
 
+import asyncio
+
 from .main import main
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
